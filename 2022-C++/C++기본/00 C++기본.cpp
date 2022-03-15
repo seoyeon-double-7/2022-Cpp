ac056c0c23b730bbc64cd4d6a9e4945c28a71fe9
cindy0817-web <s2103@e-mirim.hs.kr>
2022-03-15 12:15:43 +0900

    namespace로 축약

--- 2022-C++/C++기본/00 C++기본.cpp
+++ 2022-C++/C++기본/00 C++기본.cpp
@@ -1,9 +1,12 @@
 ﻿//std::cout를 포함하고 있음
 #include <iostream>
+//std::를 생략
+using namespace std;
 
 int main(void) 
 {
 	// std::cout 객체로 문자열을 출력
-	std::cout << "안녕 세상아\n";
+	// std::endl 객체로 개행처리
+	cout << "안녕 세상아" << endl;
 	return 0;
 }
