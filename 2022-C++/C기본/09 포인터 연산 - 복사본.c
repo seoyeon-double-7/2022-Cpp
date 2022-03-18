30cdaba7e080410c97b0fad768c4513fec753658
cindy0817-web <s2103@e-mirim.hs.kr>
2022-03-18 10:15:59 +0900

    구조체 값이 안바뀜(call by value)

---
+++ 2022-C++/C기본/09 포인터 연산 - 복사본.c
@@ -0,0 +1,26 @@
+﻿#include<stdio.h>
+
+typedef struct {
+	float x;
+	float y;
+}Point;
+
+void AddValueToPoint(Point p);
+
+int main(void)
+{
+	Point point;	// stack에 할당
+	point.x = 5.0f;
+	point.y = 10.0f;
+
+	//call by value라서 값이 안바뀜
+	AddValueToPoint(point);
+	printf("%1.f, %1.f", point.x, point.y);
+
+	return 0;
+}
+
+void AddValueToPoint(Point p) {
+	p.x += 10.0f;
+	p.y += 15.0f;
+}
