8a393caaacf77270c224a729826580c8b5d249a4
cindy0817-web <s2103@e-mirim.hs.kr>
2022-04-01 10:08:06 +0900

    매크로 함수 완성

--- 2022-C++/C++기본/02-01 매크로함수.cpp
+++ 2022-C++/C++기본/02-01 매크로함수.cpp
@@ -1,10 +1,10 @@
 ﻿#include <iostream>
 using namespace std;
 
-#define jegop(A) A * A
+#define jegop(A) ((A) * (A))
 
 int main(void)
 {
-	cout << jegop(3+1) << endl;
-	cout << 3+1 * 3+1 << endl;
+	cout << jegop(3+1) << endl;		//4*4와 같음
+	cout << (3+1) * (3+1) << endl;	// 위와 같음
 }
