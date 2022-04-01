3f03d3e96eb0be4f96809813249d41581dc8b8a8
cindy0817-web <s2103@e-mirim.hs.kr>
2022-04-01 10:04:10 +0900

    매크로 함수 기초

---
+++ 2022-C++/C++기본/02-01 매크로함수.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+
+#define jegop(A) A * A
+
+int main(void)
+{
+	cout << jegop(3) << endl;
+	cout << 3 * 3 << endl;
+}
--- 2022-C++/static 변수.cpp
+++ 2022-C++/static 변수.cpp
@@ -8,9 +8,14 @@ void jungjuk(void) {
 }
 
 int main(void) {
-	jungjuk();
-	jungjuk();
-	jungjuk();
+	//jungjuk();
+	//jungjuk();
+	//jungjuk();
+
+	int arr[4] = { 1,2,3,4 };
+	for (int i = 0; i < 4; i++) {
+		cout << arr[i] << endl;
+	}
 
 	return 0;
 }
