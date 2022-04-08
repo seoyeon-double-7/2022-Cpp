7c0326578f5bd151c1fd79d545f345f220f89cb2
SEOYEON BAE <83990943+seoyeon-double-7@users.noreply.github.com>
2022-04-08 09:43:55 +0900

    string 길이

---
+++ 2022-C++/C++기본/02-09 string 길이.cpp
@@ -0,0 +1,18 @@
+#include <iostream>	//cout, endl 사용
+#include <stdio.h>	//printf 사용
+#include <string.h>	//strlen 사용
+#include <string>	//std::string 클래스 사용
+using namespace std;
+
+int main(void) {
+
+	char cI[10] = "aaa";
+	printf("%d \n", strlen(cI));	//3
+
+	string cppI = "aaa";
+	cout << cppI.size() << endl;
+	cout << cppI.length() << endl;
+
+
+	return 0;
+}
