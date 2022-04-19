5ea5d18d2d9bcc02bdd31b98cf0f1b1023f18310
SEOYEON BAE <83990943+seoyeon-double-7@users.noreply.github.com>
2022-04-19 12:11:58 +0900

    생성자 오버로딩

--- 2022-C++/C++기본/03-01 class 생성자.cpp
+++ 2022-C++/C++기본/03-01 class 생성자.cpp
@@ -13,6 +13,7 @@ private:
 	// 반환형을 갖고 있지 않는다.
 public:
 	Student();
+	Student(int Hakbun, string Name);
 	void show();
 };
 
@@ -22,15 +23,25 @@ Student::Student()
 	sName = "이사랑";
 	cout << "학번이 등록되었습니다 히히힛 (*°▽ °*)" << endl;
 }
+
+Student::Student(int Hakbun, string Name)
+{
+	nHakbun = Hakbun;
+	sName = Name;
+	cout << "학번이 등록되었습니다 히히힛 (*°▽ °*)" << endl;
+}
 void Student::show()
 {
 	cout << "학번은 " << nHakbun << "입니다." << endl;
-	cout << "이름은 " << sName << "입니다." << endl;
+	cout << "이름은 " << sName << "입니다." << endl << endl;
 }
 
 int main(void) {
-	Student stu;
-	stu.show();
+	Student stu1 = Student();
+	stu1.show();
+
+	Student stu2 = Student(1111, "BSY");
+	stu2.show();
 
 	return 0;
 }
