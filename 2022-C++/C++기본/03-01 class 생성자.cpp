fc4878f6dce0478a47281e1287b377703eb2f219
SEOYEON BAE <83990943+seoyeon-double-7@users.noreply.github.com>
2022-05-10 10:48:23 +0900

    생성자 오버로딩

--- 2022-C++/C++기본/03-01 class 생성자.cpp
+++ 2022-C++/C++기본/03-01 class 생성자.cpp
@@ -8,14 +8,40 @@ class Student
 private:
 	int nHakbun;
 	string sName;
-	// 아래와 같은 기본생성자를 컴파일러가 만들어줌
-	// Student() {}
+
+	// 생성자 : 객체가 생성되면 자동으로 호출되는 함수
+	// 반환형을 갖고 있지 않는다.
+public:
+	Student();
+	Student(int Hakbun, string Name);
+	void show();
 };
 
+Student::Student()
+{
+	nHakbun = 1234;
+	sName = "이사랑";
+	cout << "학번이 등록되었습니다 히히힛 (*°▽ °*)" << endl;
+}
+
+Student::Student(int Hakbun, string Name)
+{
+	nHakbun = Hakbun;
+	sName = Name;
+	cout << "학번이 등록되었습니다 히히힛 (*°▽ °*)" << endl;
+}
+void Student::show()
+{
+	cout << "학번은 " << nHakbun << "입니다." << endl;
+	cout << "이름은 " << sName << "입니다." << endl << endl;
+}
+
 int main(void) {
-	// 기본 생성자
-	Student student1;
-	Student student2 = Student();
+	Student stu1 = Student();
+	stu1.show();
+
+	Student stu2 = Student(1111, "BSY");
+	stu2.show();
 
 	return 0;
 }
