d6e46c9e13f06e139fee35ecf30e26566374ad18
SEOYEON BAE <83990943+seoyeon-double-7@users.noreply.github.com>
2022-05-17 12:14:41 +0900

    복사생성자

--- 2022-C++/C++기본/03-01 ckass 생성자.cpp
+++ 2022-C++/C++기본/03-01 ckass 생성자.cpp
@@ -1,6 +1,7 @@
 #include <iostream>	// cout, endl 사용
 #include <string.h>	// strcpy() 사용
 using namespace std;
+
 class Student
 {
 private:
@@ -8,20 +9,22 @@ private:
 	char* sName;
 
 public:
+
 	// 생성자 : 객체가 생성되면 자동으로 호출되는 함수
 	// 반환형을 갖고있지 않는다
 	Student();
 	Student(int Hakbun, const char* Name);
-
-
+	Student(const Student& rhs);
 	~Student();
 
 	void show();
 
 };
+
 Student::Student()
 {
 }
+
 // 멤버변수를 초기화 할 수 있으며 따라서,
 // const형 변수와 참조형 변수를 멤버변수로 할 수 있다.
 Student::Student(int Hakbun, const char* Name)
@@ -31,6 +34,14 @@ Student::Student(int Hakbun, const char* Name)
 	int len = strlen(Name) + 1;		// 공간의 갯수 파악
 	sName = new char[len];			// 갯수만큼 메모리 할당
 	strcpy(sName, Name);
+}
+
+//직접 작성 안해도 컴파일러가 알아서 만들어주는
+//복사 생성자
+Student::Student(const Student& rhs)
+	:nHakbun(rhs.nHakbun), sName(rhs.sName)
+{
+
 }
 
 Student::~Student()
@@ -46,7 +57,13 @@ void Student::show()
 }
 int main(void)
 {
+	// "일반생성자 호출" 출력
 	Student stu1 = Student(1111, "JWP");
+	// ("1111 JWP")가 복사됨. 일반생성자 호출x
+	Student stu2 = stu1;
+
+
 	stu1.show();
+	stu2.show();
 	return 0;
 }
